25728be2fc84f068a0a67d053b8f51a1959423ec
olefard <olefard@shtrih-m.ru>
2016-11-03 13:09:19 +0300

    вынес обработку команд мониторинга в MonitoringCommands

---
+++ Source/FiscalPrinterService/src/com/shtrih/jpos/monitoring/MonitoringCommands.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.shtrih.jpos.monitoring;
+
+import com.shtrih.ej.EJActivation;
+import com.shtrih.fiscalprinter.SMFiscalPrinter;
+import com.shtrih.fiscalprinter.command.FSReadCommStatus;
+import com.shtrih.fiscalprinter.command.FSReadFiscalization;
+import com.shtrih.fiscalprinter.command.FSReadStatus;
+import com.shtrih.fiscalprinter.command.LongPrinterStatus;
+import com.shtrih.jpos.fiscalprinter.FiscalPrinterImpl;
+import com.shtrih.util.StringUtils;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+/**
+ *
+ * @author Roman
+ */
+public class MonitoringCommands {
+    public static void Help(PrintWriter out) {
+        out.println("=============================");
+        out.println("  Welcome to Shtrih JavaPOS  ");
+        out.println("=============================");
+        out.println("Commands:");
+        out.println(" STATUS");
+        out.println(" INFO");            
+        out.println(" FN");
+        out.println(" FN_UNIXTIME");
+        out.println(" CNT_QUEUE");
+        out.println(" DATE_LAST");
+        out.println(" CASH_REG regNumber");  
+        out.println(" OPER_REG regNumber");  
+        out.println("=============================");
+        out.println("");
+    }
+        
+    public static String runCommand(String command, FiscalPrinterImpl service) {
+        if (command == null || command.equals(""))
+            return "";
+        try {
+            if (command.equals("STATUS")) {
+                return getStatusText(service);
+            }
+            if (command.equals("INFO")) {
+                return getInfoText(service);
+            }
+            if (command.equals("ECTP")) {
+                return getECTPText(service);
+            }
+            if (command.equals("FN")) {
+                return getFNText(service);
+            }
+            if (command.equals("FN_UNIXTIME")) {
+                return getFNUnixTimeText(service);
+            }   
+            if (command.equals("CNT_QUEUE")) {
+                return getCntQueueText(service);
+            }
+            if (command.equals("DATE_LAST")) {
+                return getDateLastText(service);
+            }
+            if (command.startsWith("CASH_REG")) {
+                return getCashRegText(command, service);
+            }      
+            if (command.startsWith("OPER_REG")) {
+                return getOperRegText(command, service);
+            }                     
+        } catch (Exception e) {
+        }
+        return "";
+    }
+
+    private static String getStatusText(FiscalPrinterImpl service) throws Exception {
+        SMFiscalPrinter printer = service.getPrinter();
+        if (printer.getResultCode() == 0) {
+            return "OK";
+        } else {
+            return "Error " + printer.getResultText();
+        }
+    }
+
+    // INFO Модель ККМ, Зав. № ККМ, Номер ЭКЛЗ/Номер ФН
+    private static String getInfoText(FiscalPrinterImpl service) throws Exception {
+        LongPrinterStatus status = service.getLongStatus();
+        String text = service.getDeviceMetrics().getDeviceName() + ","
+                + status.getSerial() + ",";
+        if (service.getPrinter().getCapFiscalStorage())
+            text += service.getPrinter().fsReadStatus().getFsSerial();
+        else
+            text += service.getEJStatus().getSerialNumber();       
+        return text;
+    }
+
+    // ECTP Номер ЭКЛЗ, Дата активизации ЭКЛЗ, Время Активизации
+    private static String getECTPText(FiscalPrinterImpl service) throws Exception {
+        EJActivation activation = service.getEJActivation();
+        String text = activation.getEJSerial() + "," + activation.getDate()
+                + "," + activation.getTime();
+        return text;
+    }
+
+    // FN Номер ФН, Дата активизации ФН, Время Активизации ФН 
+    private static String getFNText(FiscalPrinterImpl service) throws Exception {
+        FSReadStatus status = service.getPrinter().fsReadStatus();
+        FSReadFiscalization param = service.getPrinter().fsReadFiscalization();
+        String text = status.getFsSerial().trim() + "," + param.getDate() + ","
+                + param.getTime();
+        return text;
+    }
+
+    // FN_UNIXTIME Дата активизации ФН в UNIXTIME
+    private static String getFNUnixTimeText(FiscalPrinterImpl service) throws Exception {
+        FSReadFiscalization param = service.getPrinter().fsReadFiscalization();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(param.getDate().getYear() + 2000, param.getDate().getMonth() - 1, 
+                param.getDate().getDay(), param.getTime().getHour(), 
+                param.getTime().getMin(), param.getTime().getSec());
+        calendar.set(Calendar.MILLISECOND, 0);
+        String text = String.valueOf(calendar.getTimeInMillis() / 1000L);
+        return text;
+    }
+
+    // CNT_QUEUE Кол-во документов, ожидающих отправки в ОФД (очередь)
+    private static String getCntQueueText(FiscalPrinterImpl service) throws Exception {
+        FSReadCommStatus status = service.getPrinter().fsReadCommStatus();
+        String text = String.valueOf(status.getQueueSize());
+        return text;
+    }
+
+    // DATE_LAST Дата последнего отправленного документа в ОФД
+    private static String getDateLastText(FiscalPrinterImpl service) throws Exception {
+        FSReadCommStatus status = service.getPrinter().fsReadCommStatus();
+        String text = status.getDocumentDate() + "," + status.getDocumentTime();
+        return text;
+    }
+
+    
+    // CASH_REG Номер регистра
+    // Содержимое денежного регистра
+    private static String getCashRegText(String command, FiscalPrinterImpl service) throws Exception {
+        String[] params = StringUtils.split(command, ' ');
+        if (params.length < 2)
+            return "WRONG_PARAM";
+        long reg = service.getPrinter().readCashRegister(Integer.parseInt(params[1]));
+        String text = String.valueOf(reg);
+        return text;
+    }
+
+    // OPER_REG Номер регистра
+    // Содержимое операционного регистра
+    private static String getOperRegText(String command, FiscalPrinterImpl service) throws Exception {
+        String[] params = StringUtils.split(command, ' ');
+        if (params.length < 2)
+            return "WRONG_PARAM";
+        int reg = service.getPrinter().readOperationRegister(Integer.parseInt(params[1]));
+        String text = String.valueOf(reg);
+        return text;
+    }    
+}
--- Source/FiscalPrinterService/src/com/shtrih/jpos/monitoring/MonitoringServer.java
+++ Source/FiscalPrinterService/src/com/shtrih/jpos/monitoring/MonitoringServer.java
@@ -10,26 +10,13 @@ package com.shtrih.jpos.monitoring;
  * @author V.Kravtsov
  */
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import com.shtrih.util.CompositeLogger;
-import com.shtrih.ej.EJActivation;
-import com.shtrih.fiscalprinter.SMFiscalPrinter;
-import com.shtrih.fiscalprinter.command.FSReadCommStatus;
-import com.shtrih.fiscalprinter.command.FSReadFiscalization;
-import com.shtrih.fiscalprinter.command.FSReadStatus;
-import com.shtrih.fiscalprinter.command.LongPrinterStatus;
 import com.shtrih.jpos.fiscalprinter.FiscalPrinterImpl;
-import com.shtrih.util.StringUtils;
 import java.io.PrintWriter;
-import java.util.Calendar;
-import java.util.Date;
 
 public class MonitoringServer implements Runnable {
 
@@ -84,38 +71,22 @@ public class MonitoringServer implements Runnable {
                 BufferedReader reader = new BufferedReader(
                         new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                Hello(out);
+                MonitoringCommands.Help(out);
                 while(true) {
                     String cmd = reader.readLine();
                     if (cmd == null)
                         continue;
-                    String answer = runCommand(cmd);
+                    String answer = MonitoringCommands.runCommand(cmd, service);
                     out.println(answer);                
                 }
             } catch (IOException e) {
             }
         }
         
-        private void Hello(PrintWriter out) {
-            out.println("=============================");
-            out.println("  Welcome to Shtrih JavaPOS  ");
-            out.println("=============================");
-            out.println("Commands:");
-            out.println(" STATUS");
-            out.println(" INFO");            
-            out.println(" FN");
-            out.println(" FN_UNIXTIME");
-            out.println(" CNT_QUEUE");
-            out.println(" DATE_LAST");
-            out.println(" CASH_REG regNumber");  
-            out.println(" OPER_REG regNumber");  
-            out.println("=============================");
-            out.println("");
-        }
-        
         Socket socket = null;
     }
     
+    @Override
     public void run() {
         try {
             while (true){
@@ -127,119 +98,4 @@ public class MonitoringServer implements Runnable {
             logger.error("run, ", e);
         }
     }
-
-    private String runCommand(String command) {
-        if (command == null || command.equals(""))
-            return "";
-        try {
-            if (command.equals("STATUS")) {
-                return getStatusText();
-            }
-            if (command.equals("INFO")) {
-                return getInfoText();
-            }
-            if (command.equals("ECTP")) {
-                return getECTPText();
-            }
-            if (command.equals("FN")) {
-                return getFNText();
-            }
-            if (command.equals("FN_UNIXTIME")) {
-                return getFNUnixTimeText();
-            }   
-            if (command.equals("CNT_QUEUE")) {
-                return getCntQueueText();
-            }
-            if (command.equals("DATE_LAST")) {
-                return getDateLastText();
-            }
-            if (command.startsWith("CASH_REG")) {
-                return getCashRegText(command);
-            }      
-            if (command.startsWith("OPER_REG")) {
-                return getOperRegText(command);
-            }                     
-        } catch (Exception e) {
-        }
-        return "";
-    }
-
-    public String getStatusText() throws Exception {
-        SMFiscalPrinter printer = service.getPrinter();
-        if (printer.getResultCode() == 0) {
-            return "OK";
-        } else {
-            return "Error " + printer.getResultText();
-        }
-    }
-
-    // INFO Модель ККМ, Зав. № ККМ, Номер ЭКЛЗ
-    public String getInfoText() throws Exception {
-        LongPrinterStatus status = service.getLongStatus();
-        String text = service.getDeviceMetrics().getDeviceName() + ","
-                + status.getSerial() + ",";
-        if (service.getPrinter().getCapFiscalStorage())
-            text += service.getPrinter().fsReadStatus().getFsSerial();
-        else
-            text += service.getEJStatus().getSerialNumber();       
-        return text;
-    }
-
-    // ECTP Номер ЭКЛЗ, Дата активизации ЭКЛЗ, Время Активизации
-    public String getECTPText() throws Exception {
-        EJActivation activation = service.getEJActivation();
-        String text = activation.getEJSerial() + "," + activation.getDate()
-                + "," + activation.getTime();
-        return text;
-    }
-
-    private String getFNText() throws Exception {
-        FSReadStatus status = service.getPrinter().fsReadStatus();
-        FSReadFiscalization param = service.getPrinter().fsReadFiscalization();
-        String text = status.getFsSerial().trim() + "," + param.getDate() + ","
-                + param.getTime();
-        return text;
-    }
-
-    private String getFNUnixTimeText() throws Exception {
-        FSReadFiscalization param = service.getPrinter().fsReadFiscalization();
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(param.getDate().getYear() + 2000, param.getDate().getMonth() - 1, 
-                param.getDate().getDay(), param.getTime().getHour(), 
-                param.getTime().getMin(), param.getTime().getSec());
-        calendar.set(Calendar.MILLISECOND, 0);
-        String text = String.valueOf(calendar.getTimeInMillis() / 1000L);
-        return text;
-    }
-
-    private String getCntQueueText() throws Exception {
-        FSReadCommStatus status = service.getPrinter().fsReadCommStatus();
-        String text = String.valueOf(status.getQueueSize());
-        return text;
-    }
-
-    private String getDateLastText() throws Exception {
-        FSReadCommStatus status = service.getPrinter().fsReadCommStatus();
-        String text = status.getDocumentDate() + "," + status.getDocumentTime();
-        return text;
-    }
-
-    private String getCashRegText(String command) throws Exception {
-        String[] params = StringUtils.split(command, ' ');
-        if (params.length < 2)
-            return "WRONG_PARAM";
-        long reg = service.getPrinter().readCashRegister(Integer.parseInt(params[1]));
-        String text = String.valueOf(reg);
-        return text;
-    }
-
-    private String getOperRegText(String command) throws Exception {
-        String[] params = StringUtils.split(command, ' ');
-        if (params.length < 2)
-            return "WRONG_PARAM";
-        int reg = service.getPrinter().readOperationRegister(Integer.parseInt(params[1]));
-        String text = String.valueOf(reg);
-        return text;
-    }
-
 }
