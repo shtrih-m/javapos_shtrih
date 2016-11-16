7949d4119e911a7497827bfe14a6ceca8a51b956
Alexey Mednyy <swexru@gmail.com>
2016-11-16 14:41:27 +0300

    version 313

--- Source/FiscalPrinterService/src/com/shtrih/jpos/fiscalprinter/FiscalPrinterImpl.java
+++ Source/FiscalPrinterService/src/com/shtrih/jpos/fiscalprinter/FiscalPrinterImpl.java
@@ -467,7 +467,7 @@ public class FiscalPrinterImpl extends DeviceService implements PrinterConst,
         totalizerType = FPTR_TT_DAY;
         capUpdateStatistics = true;
         capStatisticsReporting = true;
-        deviceServiceVersion = deviceVersion113 + 312;
+        deviceServiceVersion = deviceVersion113 + 313;
         freezeEvents = true;
     }
 
