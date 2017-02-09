2bb826e14547821ab222181c7ae36e3fe0c8c0c6
Pavel Zhirkov <zhirkov@shtrih-m.ru>
2017-02-09 19:22:09 +0300

    Removed NotNull annotation from ByteUtils

--- Source/FiscalPrinterService/test/com/shtrih/util/ByteUtils.java
+++ Source/FiscalPrinterService/test/com/shtrih/util/ByteUtils.java
@@ -1,12 +1,10 @@
 package com.shtrih.util;
 
-import com.sun.istack.internal.NotNull;
-
 /**
  * @author P.Zhirkov
  */
 public class ByteUtils {
-    public static byte[] byteArray(@NotNull int... array) {
+    public static byte[] byteArray(int... array) {
         byte[] result = new byte[array.length];
 
         for (int i = 0; i < array.length; i++) {
