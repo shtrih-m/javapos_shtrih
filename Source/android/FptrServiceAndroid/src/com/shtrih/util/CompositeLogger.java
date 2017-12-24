4631bcf448a735416ebf0dd89d6725954cc7e3f6
Pavel Zhirkov <zhirkov@shtrih-m.ru>
2017-12-25 02:56:13 +0300

    На Андроиде настройка logEnabled теперь усправляет выводом лога в Logcat

--- Source/android/FptrServiceAndroid/src/com/shtrih/util/CompositeLogger.java
+++ Source/android/FptrServiceAndroid/src/com/shtrih/util/CompositeLogger.java
@@ -2,58 +2,68 @@ package com.shtrih.util;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import android.util.Log;
 
 /**
- *
  * @author V.Kravtsov
  */
-
 public class CompositeLogger {
-    
+
     private final Logger log4JLogger;
     private final String className;
+
+    private boolean isEnabled;
 
     private CompositeLogger(String className) {
         log4JLogger = LoggerFactory.getLogger(className);
         this.className = className;
     }
-    
+
     public static synchronized CompositeLogger getLogger(java.lang.Class c) {
         return getLogger(c.getName());
     }
-    
+
     public static synchronized CompositeLogger getLogger(String className) {
         return new CompositeLogger(className);
     }
-    
+
     public void setEnabled(boolean value) {
-
+        isEnabled = value;
     }
-    
+
     public synchronized void fatal(String text, Throwable e) {
-        //Log.e(className, text + " " + e);
-        log4JLogger.error(text, e);
-    }
-    
-    public synchronized void error(String text, Throwable e) {
+        if (isEnabled)
+            Log.e(className, text + "" + e);
 
-        //Log.e(className, text + " " + e);
         log4JLogger.error(text, e);
     }
-    
+
+    public synchronized void error(String text, Throwable e) {
+        if (isEnabled)
+            Log.e(className, text + "" + e);
+
+        log4JLogger.error(text, e);
+    }
+
     public synchronized void error(Throwable e) {
-        //Log.e(className, "" + e);
+        if (isEnabled)
+            Log.e(className, "" + e);
+
         log4JLogger.error("Error", e);
     }
-    
+
     public synchronized void error(String text) {
-        //Log.e(className, text);
+        if (isEnabled)
+            Log.e(className, text);
+
         log4JLogger.error(text);
     }
-    
+
     public synchronized void debug(String text) {
-        //Log.d(className, text);
+        if (isEnabled)
+            Log.d(className, text);
+
         log4JLogger.debug(text);
     }
 }
