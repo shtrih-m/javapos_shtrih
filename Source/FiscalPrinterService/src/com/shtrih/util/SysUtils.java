c985da3cf0db59a8b931206c69ab89d8ac773795
Pavel Zhirkov <zhirkov@shtrih-m.ru>
2018-01-24 13:23:19 +0300

    Добавлен сеттер для пути к файлам в SysUtils

--- Source/FiscalPrinterService/src/com/shtrih/util/SysUtils.java
+++ Source/FiscalPrinterService/src/com/shtrih/util/SysUtils.java
@@ -3,12 +3,17 @@ package com.shtrih.util;
 
 public class SysUtils {
 
+    private static String filePath = "";
+
     public static String getFilesPath() {
-        return "";
+        return filePath;
     }
 
-    public static void sleep(long millis) throws InterruptedException 
-    {
+    public static void setFilePath(String value) {
+        filePath = value;
+    }
+
+    public static void sleep(long millis) throws InterruptedException {
         Thread.sleep(millis);
     }
 }
