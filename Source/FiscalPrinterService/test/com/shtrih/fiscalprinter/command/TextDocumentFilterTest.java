8c32f1c0f94602fcbe6438d066900eb077e37ead
Vitaly Kravtsov <vitalykravtsov@gmail.com>
2023-11-28 07:03:46 +0300

    [-] Minor changes

--- Source/FiscalPrinterService/test/com/shtrih/fiscalprinter/command/TextDocumentFilterTest.java
+++ Source/FiscalPrinterService/test/com/shtrih/fiscalprinter/command/TextDocumentFilterTest.java
@@ -16,11 +16,15 @@ import com.shtrih.jpos.fiscalprinter.FptrParameters;
  */
 public class TextDocumentFilterTest {
 
+    private final TextDocumentFilter filter = new TextDocumentFilter();
+    private final FptrParameters params = new FptrParameters();
+    private final SMFiscalPrinterNull printer = new SMFiscalPrinterNull(params);
+    
+    
     @Test
     public void should_serialize_request() throws Exception {
-        TextDocumentFilter item = new TextDocumentFilter();
-        assertEquals("0.123", item.quantityToStr(0.123));
-        assertEquals("0.123456", item.quantityToStr(0.123456));
+        assertEquals("0.123", filter.quantityToStr(0.123));
+        assertEquals("0.123456", filter.quantityToStr(0.123456));
     }
 
     @Test
@@ -29,10 +33,7 @@ public class TextDocumentFilterTest {
         PrintCashIn command = new PrintCashIn();
         command.setReceiptNumber(123);
         command.setAmount(12345);
-        FptrParameters params = new FptrParameters();
-        SMFiscalPrinterNull printer = new SMFiscalPrinterNull(params);
         printer.getOperationRegisters().put(new Integer(155), new Integer(1234));
-        TextDocumentFilter filter = new TextDocumentFilter();
         assertEquals(0, filter.getLines().size());
                 
         filter.init(printer);
@@ -49,18 +50,173 @@ public class TextDocumentFilterTest {
         PrintCashOut command = new PrintCashOut();
         command.setReceiptNumber(123);
         command.setAmount(12345);
-        FptrParameters params = new FptrParameters();
-        SMFiscalPrinterNull printer = new SMFiscalPrinterNull(params);
         printer.getOperationRegisters().put(new Integer(156), new Integer(1234));
-        TextDocumentFilter filter = new TextDocumentFilter();
         assertEquals(0, filter.getLines().size());
                 
         filter.init(printer);
         filter.setEnabled(true);
         filter.afterCommand(command);
         assertEquals(2, filter.getLines().size());
-        assertEquals("ВЫПЛАТА                        1234", filter.getLines().get(0));
-        assertEquals("                             123.45", filter.getLines().get(1));
+        assertEquals("ВЫПЛАТА                         1234", filter.getLines().get(0));
+        assertEquals("                              123.45", filter.getLines().get(1));
     }
+    
+    @Test
+    public void test_print_sale_command() throws Exception 
+    {
+        PriceItem item = new PriceItem();
+        item.setDepartment(1);
+        item.setPaymentType(0);
+        item.setPrice(123456);
+        item.setQuantity(1);
+        item.setSubjectType(0);
+        item.setTax1(1);
+        item.setTax2(0);
+        item.setTax3(0);
+        item.setTax4(0);
+        item.setText("Item 1");
+        PrintSale command = new PrintSale(30, item);
+        printer.getOperationRegisters().put(new Integer(156), new Integer(1234));
+        filter.receiptEnabled = true;
+        assertEquals(0, filter.getLines().size());
+                
+        filter.init(printer);
+        filter.setEnabled(true);
+        filter.afterCommand(command);
+        assertEquals(3, filter.getLines().size());
+        assertEquals("                               №0001", filter.getLines().get(0));
+        assertEquals("Item 1                              ", filter.getLines().get(1));
+        assertEquals("01                        =1234.56_А", filter.getLines().get(2));
+    }
+    
+    @Test
+    public void test_print_refund_command() throws Exception 
+    {
+        PriceItem item = new PriceItem();
+        item.setDepartment(1);
+        item.setPaymentType(0);
+        item.setPrice(123456);
+        item.setQuantity(1);
+        item.setSubjectType(0);
+        item.setTax1(1);
+        item.setTax2(0);
+        item.setTax3(0);
+        item.setTax4(0);
+        item.setText("Item 1");
+        PrintRefund command = new PrintRefund(30, item);
+        printer.getOperationRegisters().put(new Integer(156), new Integer(1234));
+        filter.receiptEnabled = true;
+        assertEquals(0, filter.getLines().size());
+                
+        filter.init(printer);
+        filter.setEnabled(true);
+        filter.afterCommand(command);
+        assertEquals(3, filter.getLines().size());
+        assertEquals("                               №0001", filter.getLines().get(0));
+        assertEquals("Item 1                              ", filter.getLines().get(1));
+        assertEquals("01                        =1234.56_А", filter.getLines().get(2));
+    }
+    
+    @Test
+    public void test_print_void_sale_command() throws Exception 
+    {
+        PriceItem item = new PriceItem();
+        item.setDepartment(1);
+        item.setPaymentType(0);
+        item.setPrice(123456);
+        item.setQuantity(1);
+        item.setSubjectType(0);
+        item.setTax1(1);
+        item.setTax2(0);
+        item.setTax3(0);
+        item.setTax4(0);
+        item.setText("Item 1");
+        PrintVoidSale command = new PrintVoidSale(30, item);
+        printer.getOperationRegisters().put(new Integer(156), new Integer(1234));
+        filter.receiptEnabled = true;
+        assertEquals(0, filter.getLines().size());
+                
+        filter.init(printer);
+        filter.setEnabled(true);
+        filter.afterCommand(command);
+        assertEquals(3, filter.getLines().size());
+        assertEquals("                               №0001", filter.getLines().get(0));
+        assertEquals("Item 1                              ", filter.getLines().get(1));
+        assertEquals("01                        =1234.56_А", filter.getLines().get(2));
+    }
+    
+    @Test
+    public void test_print_void_refund_command() throws Exception 
+    {
+        PriceItem item = new PriceItem();
+        item.setDepartment(1);
+        item.setPaymentType(0);
+        item.setPrice(123456);
+        item.setQuantity(1);
+        item.setSubjectType(0);
+        item.setTax1(1);
+        item.setTax2(0);
+        item.setTax3(0);
+        item.setTax4(0);
+        item.setText("Item 1");
+        PrintVoidRefund command = new PrintVoidRefund(30, item);
+        printer.getOperationRegisters().put(new Integer(156), new Integer(1234));
+        filter.receiptEnabled = true;
+        assertEquals(0, filter.getLines().size());
+                
+        filter.init(printer);
+        filter.setEnabled(true);
+        filter.afterCommand(command);
+        assertEquals(3, filter.getLines().size());
+        assertEquals("                               №0001", filter.getLines().get(0));
+        assertEquals("Item 1                              ", filter.getLines().get(1));
+        assertEquals("01                        =1234.56_А", filter.getLines().get(2));
+    }
+    
+    
+    @Test
+    public void test_print_sale_command2() throws Exception 
+    {
+        PriceItem item = new PriceItem();
+        item.setDepartment(1);
+        item.setPaymentType(0);
+        item.setPrice(123456);
+        item.setQuantity(1.234);
+        item.setSubjectType(0);
+        item.setTax1(1);
+        item.setTax2(0);
+        item.setTax3(0);
+        item.setTax4(0);
+        item.setText("Item 1");
+        PrintSale command = new PrintSale(30, item);
+        printer.getOperationRegisters().put(new Integer(156), new Integer(1234));
+        filter.receiptEnabled = true;
+        assertEquals(0, filter.getLines().size());
+                
+        filter.init(printer);
+        filter.setEnabled(true);
+        filter.afterCommand(command);
+        assertEquals(4, filter.getLines().size());
+        assertEquals("                               №0001", filter.getLines().get(0));
+        assertEquals("Item 1                              ", filter.getLines().get(1));
+        assertEquals("                     1.234 X 1234.56", filter.getLines().get(2));
+        assertEquals("01                        =1523.45_А", filter.getLines().get(3));
+    }
+    
+    @Test
+    public void test_void_receipt_command() throws Exception 
+    {
+        VoidFiscalReceipt command = new VoidFiscalReceipt();
+        filter.receiptEnabled = true;
+        assertEquals(0, filter.getLines().size());
+                
+        filter.init(printer);
+        filter.setEnabled(true);
+        filter.afterCommand(command);
+        assertEquals(1, filter.getLines().size());
+        assertEquals("ЧЕК АННУЛИРОВАН                     ", filter.getLines().get(0));
+    }
+
+        
 }
 
