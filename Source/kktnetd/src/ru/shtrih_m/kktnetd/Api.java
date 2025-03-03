7dbd61d42e91f4fd6328d132e2a7fdfa17e8c1bf
Vitaly Kravtsov <vitalykravtsov@gmail.com>
2025-03-03 11:10:59 +0300

    [+] Updated PPP library kktnetd for ARM 32 and 64 platforms

--- Source/kktnetd/src/ru/shtrih_m/kktnetd/Api.java
+++ Source/kktnetd/src/ru/shtrih_m/kktnetd/Api.java
@@ -1,13 +1,9 @@
 package ru.shtrih_m.kktnetd;
 
-public class Api
-{
-    /*
+public class Api{
     static{
         System.loadLibrary("kktnetd");
     }
-    */
-    
     //the function is defined in a c-file
     public static native int start(final String name);
     public static native void stop();
@@ -15,5 +11,6 @@ public class Api
     public static native long api_init(final String instance_config);
     public static native long api_run(long ctx);
     public static native void api_stop(long ctx);
-    public static native void api_deinit(long ctx);    
+    public static native void api_deinit(long ctx);
+    public static native String api_status(long ctx);
 }
